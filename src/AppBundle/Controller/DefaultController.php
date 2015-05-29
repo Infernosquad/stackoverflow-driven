5dbdcf850424c6fc29a1e29519e34a6a0b54ef31
infernosquad <infzanoza@gmail.com>
2015-05-29 19:53:49 +0500

    Fixed upload

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,28 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+
+        $request = $this->getRequest();
+        if($request->getMethod() == "POST"){
+            $uploadPath = $this->container->getParameter('kernel.root_dir') . '/../web/uploads/';
+
+            try {
+                $uploadedAvatarFile = $request->files->get('avatar');
+
+                /* @var $avatarFile \Symfony\Component\HttpFoundation\File\File */
+                $avatarFile = $uploadedAvatarFile->move($uploadPath, $uploadedAvatarFile->getClientOriginalName());
+
+                unset($uploadedAvatarFile);
+            } catch (\Exception $e) {
+                /* if you don't set $avatarFile to a default file here
+                 * you cannot execute the next instruction.
+                 */
+            }
+
+            $avatarExt = $avatarFile->getExtension();
+            var_dump($avatarExt);
+
+        }
 
         return $this->render('AppBundle:Default:index.html.twig',array('users' => $users));
     }
