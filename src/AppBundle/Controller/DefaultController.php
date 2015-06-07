33811d900ed3ea21030fa31871696dc7dfa2c283
infernosquad <infzanoza@gmail.com>
2015-06-08 00:38:00 +0500

    Logger service

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,17 +2,28 @@
 
 namespace AppBundle\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+
+//        $this->get('logger')->info('message');
+        $this->logger->info('new message');
 
         return $this->render('AppBundle:Default:index.html.twig',array('users' => $users));
     }
