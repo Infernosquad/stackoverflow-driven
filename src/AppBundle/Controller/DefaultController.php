44829fff927772456a17cedaaf0340d041d74648
infernosquad <infzanoza@gmail.com>
2015-06-08 00:51:17 +0500

    Add test form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,18 +2,39 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Form\Type\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
 
-        return $this->render('AppBundle:Default:index.html.twig',array('users' => $users));
+        $form = $this->createForm(new UserType(),new User());
+
+        $form->handleRequest($request);
+
+        if($request->isMethod('POST'))
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($form->getData());
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('AppBundle:Default:index.html.twig', array(
+                'users' => $users,
+                'form'  => $form->createView()
+            )
+        );
     }
 }
